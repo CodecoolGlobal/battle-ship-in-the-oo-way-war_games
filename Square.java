e7adb92eb9ebef78566924961b73445ef7f67936
Flloki5 <rozli@op.pl>
2020-05-24 13:06:26 +0200

    Added Square methods

--- Square.java
+++ Square.java
@@ -1,8 +1,41 @@
 package Battleships;
 
+import java.util.ArrayList;
+
+
 public class Square {
+
     private boolean isHit;
     private Ship ship;
 
-    
+    public Square() {
+        this.isHit = false;
+    }
+
+    public boolean getIsHit(){
+        return isHit;
+    }
+
+    public Ship getShip(){
+        return ship;
+    }
+
+    public void markHit() {
+        this.isHit = true;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    public boolean hasShip(){
+        if(this.ship != null){
+            return true;
+        }else return false;
+    }
+
+    @Override
+    public String toString() {
+        return hasShip() ? "X" : " "; 
+    }
 }
