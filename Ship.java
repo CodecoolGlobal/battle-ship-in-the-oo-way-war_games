0677dcbbc130499f3ff9c6b554f571efb80e304b
Michal Bartosik <msbartosik@gmail.com>
2020-05-24 12:08:24 +0200

    add getters methods

--- Ship.java
+++ Ship.java
@@ -1,13 +1,26 @@
-package Battleships;
 
 public class Ship {
-    int x;
-    int y;
-    int length;
-    String direction;
 
-    public Ship(int length, String direction){
+    private int x;
+    private int y;
+    private int length;
+    private String direction;
+
+    public Ship(int length, String direction) {
         this.length = length;
         this.direction = direction;
     }
+
+    public int[] getCoords() {
+        int[] coords = {this.x, this.y};
+        return coords;
+    }
+
+    public int getlength() {
+        return this.length;
+    }
+
+    public String getdirection() {
+        return this.direction;
+    } 
 }
