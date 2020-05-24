d310c08a8a8a46d9e2a89b1e8e3865e9462f0a3c
wjtkwilk@gmail.com <wjtkwilk@gmail.com>
2020-05-24 12:00:21 +0200

    Add Ocean class.

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 package Battleships;
 
+import java.util.ArrayList;
+
 public class Main {
+    static Ocean ocean;
+    static ArrayList<Ship> ships;
+
     public static void main(String[] args) {
-        
+        ocean = new Ocean();
     }
 }
--- Ocean.java
+++ Ocean.java
@@ -1,5 +1,22 @@
 package Battleships;
 
+import java.util.ArrayList;
+
 public class Ocean {
-    
+    private ArrayList<ArrayList<Square>> Ocean;
+    final private int WIDTH = 10;
+    final private int HEIGHT = 10;
+
+    public Ocean(){
+        this.Ocean = new ArrayList<ArrayList<Square>>();
+        for (int i=0; i<HEIGHT; i++){
+            Ocean.add(new ArrayList<Square>());
+        }
+
+        for (ArrayList<Square> line : Ocean) {
+            for (int i=0; i<WIDTH; i++){
+                line.add(new Square());
+            }
+        }
+    }
 }
