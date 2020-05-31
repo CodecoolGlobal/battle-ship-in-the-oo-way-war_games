2f25eb08c47528d93962c53aacb56b8eec431999
Michal Bartosik <msbartosik@gmail.com>
2020-05-31 17:24:11 +0200

    fixed indentation

--- SetupController.java
+++ SetupController.java
@@ -26,35 +26,34 @@ public class SetupController {
             view.clearDisplay();
             view.displayBoard(ocean.getMap());
 
-                boolean validInput = false;
+            boolean validInput = false;
 
-                while (!validInput) {
-                    view.printMessage("\nEnter " + key + " coordinates (" + shipsDict.get(key) + "): ");          
-                    coords = Util.validateUserInput(coordsPattern, coords, "\nEnter valid coordinates: ");
+            while (!validInput) {
+                view.printMessage("\nEnter " + key + " coordinates (" + shipsDict.get(key) + "): ");          
+                coords = Util.validateUserInput(coordsPattern, coords, "\nEnter valid coordinates: ");
 
-                    if (counter < 4) {
-                        view.printMessage("\nIs horizontal? [Y/N]: ");
-                        position = Util.validateUserInput(positionPattern, position, "\nType Y or N: ");            
-                        position = Util.getPositionFromInput(position);
-                        Ship ship = new Ship(key, position);
+                if (counter < 4) {
+                    view.printMessage("\nIs horizontal? [Y/N]: ");
+                    position = Util.validateUserInput(positionPattern, position, "\nType Y or N: ");            
+                    position = Util.getPositionFromInput(position);
+                    Ship ship = new Ship(key, position);
 
-                        if (ocean.checkIfWithinBounds(ship, coords) && ocean.checkIfSpaceFreeForShip(ship, coords)) {
-                            ships.add(ship);
-                            validInput = true;
-                        } else {
-                            System.out.println("\nShips cannot be placed outside the board, overlap or touch");
-                        }
+                    if (ocean.checkIfWithinBounds(ship, coords) && ocean.checkIfSpaceFreeForShip(ship, coords)) {
+                        ships.add(ship);
+                        validInput = true;
                     } else {
-                        Ship ship = new Ship(key, "horizontal");
-                        if (!ocean.checkIfSpaceFreeForShip(ship, coords)) {
-                            System.out.println("\nShips cannot overlap or touch");
-                        } else {
-                            ships.add(ship);
-                            validInput = true;
-                        }
-                            
+                        System.out.println("\nShips cannot be placed outside the board, cannot overlap or touch");
                     }
-                }        
+                } else {
+                    Ship ship = new Ship(key, "horizontal");
+                    if (!ocean.checkIfSpaceFreeForShip(ship, coords)) {
+                        System.out.println("\nShips cannot overlap or touch");
+                    } else {
+                        ships.add(ship);
+                        validInput = true;
+                    }
+                }
+            }        
 
             ocean.addShip(ships.get(counter), coords);
             counter++;
