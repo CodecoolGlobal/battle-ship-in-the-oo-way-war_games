bb6390f5c8ff06fca2b5c3768b19f268bb02130e
Flloki5 <rozli@op.pl>
2020-06-06 16:07:28 +0200

    Add medium logic

--- Ai.java
+++ Ai.java
@@ -16,10 +16,19 @@ public class Ai {
     public String getCoordinatesToShoot(Ocean ocean){
         ArrayList<ArrayList<Square>> map = ocean.getMap();
         ArrayList<Square> targets = new ArrayList<Square>();
-        
-        for (ArrayList<Square> row : map) {
-            for (Square square : row) {
-                if(!square.getIsHit()) square.markIsGoodTarget();
+        if(difficultyLevel.equals("easy") || !hasTargets(map)){
+            for (ArrayList<Square> row : map) {
+                for (Square square : row) {
+                    if(!square.getIsHit()) square.markIsGoodTarget();
+                }
+            }
+        }
+
+        if(difficultyLevel.equals("medium")){
+            for (ArrayList<Square> row : map) {
+                for (Square square : row) {
+                    if(hasSunkNeighbour(ocean, square)) square.unmarkIsGoodTarget();
+                }
             }
         }
 
@@ -38,5 +47,32 @@ public class Ai {
         int randomIndex = randomGenerator.nextInt(targets.size());
 
         return targets.get(randomIndex);
+    }
+
+    private boolean hasTargets(ArrayList<ArrayList<Square>> map){
+        for (ArrayList<Square> row : map) {
+            for (Square square : row) {
+                if(square.getIsGoodTarget()) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasSunkNeighbour(Ocean ocean, Square square){
+        for (Square neighbour : getNeighbourSquares(ocean, square)) {
+            if(neighbour.getShip().getIsSunk()) return true;
+        }
+        return false;
+    }
+
+    private ArrayList<Square> getNeighbourSquares(Ocean ocean, Square square){
+        int[] coord = ocean.getSquareLocation(square.getCoords());
+        ArrayList<Square> neighbours = new ArrayList<Square>();
+        if((coord[0] - 1) >= 0) neighbours.add(ocean.getMap().get(coord[0] - 1).get(coord[1]));
+        if((coord[0] + 1) <= 9) neighbours.add(ocean.getMap().get(coord[0] + 1).get(coord[1]));
+        if((coord[1] - 1) >= 0) neighbours.add(ocean.getMap().get(coord[0]).get(coord[1] - 1));
+        if((coord[1] + 1) <= 9) neighbours.add(ocean.getMap().get(coord[0]).get(coord[1] + 1));
+        
+        return neighbours;
     }
 }
